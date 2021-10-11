ce4d30ad4272f3eeb9da59eee139e79891afbf63
felu98 <65612488+felu98@users.noreply.github.com>
2021-10-11 17:07:33 -0300

    cambio de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,23 +3,23 @@ import 'package:greencycle/screens/home.dart';
 import 'package:greencycle/screens/onboarding.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GreenCycle',
-      theme: ThemeData(fontFamily: 'Railway'),
-      initialRoute: "/home",
-      routes: <String, WidgetBuilder>{
-        "/onboarding": (BuildContext context) => new Onboarding(),
-        "/home" : (BuildContext context) => new Home(),
-      });
+        title: 'GreenCycle',
+        theme: ThemeData(fontFamily: 'Railway'),
+        initialRoute: "/home",
+        routes: <String, WidgetBuilder>{
+          "/onboarding": (BuildContext context) => new Onboarding(),
+          "/home": (BuildContext context) => new Home(),
+        });
   }
 }
