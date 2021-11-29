776ad43a8662808d323af8f017c9b5aabcffa194
florencia chao <fchao@itba.edu.ar>
2021-11-29 17:34:41 -0300

    arreglo register front - herni habia hecho el back

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -21,6 +21,71 @@ class _RegisterState extends State<Register> {
   final TextEditingController? _passwordController = TextEditingController();
   final TextEditingController? _confirmPasswordController = TextEditingController();
 
+  String? get _errorTexEmail {
+    // at any time, we can get the text from _controller.value.text
+    final text = _emailController!.value.text;
+    // Note: you can do your own custom validation here
+    // Move this logic this outside the widget for more testable code
+    if (text.isEmpty) {
+      return 'El mail es un campo obligatorio';
+    }
+    // return null if the text is valid
+    return null;
+  }
+  String? get _errorTexFirstName {
+    // at any time, we can get the text from _controller.value.text
+    final text = _firstNameController!.value.text;
+    // Note: you can do your own custom validation here
+    // Move this logic this outside the widget for more testable code
+    if (text.isEmpty) {
+      return 'El nombre es un campo obligatorio';
+    }
+    // return null if the text is valid
+    return null;
+  }
+  String? get _errorTexLastName {
+    // at any time, we can get the text from _controller.value.text
+    final text = _lastNameController!.value.text;
+    // Note: you can do your own custom validation here
+    // Move this logic this outside the widget for more testable code
+    if (text.isEmpty) {
+      return 'El nombre es un campo obligatorio';
+    }
+    // return null if the text is valid
+    return null;
+  }
+  String? get _errorTexPassword {
+    // at any time, we can get the text from _controller.value.text
+    final text = _passwordController!.value.text;
+    final text2 = _confirmPasswordController!.value.text;
+    // Note: you can do your own custom validation here
+    // Move this logic this outside the widget for more testable code
+    if (text.isEmpty) {
+      return 'El mail es un campo obligatorio';
+    }
+    if(text != text2){
+      return 'Las contraseñas deben coincidir';
+    }
+    // return null if the text is valid
+    return null;
+  }
+
+  String? get _errorTexConfirmPassword {
+    // at any time, we can get the text from _controller.value.text
+    final text = _passwordController!.value.text;
+    final text2 = _confirmPasswordController!.value.text;
+    // Note: you can do your own custom validation here
+    // Move this logic this outside the widget for more testable code
+    if (text2.isEmpty) {
+      return 'El mail es un campo obligatorio';
+    }
+    if(text != text2){
+      return 'Las contraseñas deben coincidir';
+    }
+    // return null if the text is valid
+    return null;
+  }
+
   // Podria hacerlo con Regex pero soy vago
   bool validEmail(TextEditingController? controller) {
     if (controller == null) return false;
@@ -37,8 +102,7 @@ class _RegisterState extends State<Register> {
             SafeArea(
               child: ListView(children: [
                 Padding(
-                  padding: const EdgeInsets.only(
-                      top: 16, left: 24.0, right: 24.0, bottom: 32),
+                  padding: const EdgeInsets.only(left: 24.0, right: 24.0),
                       child: Column(
                         children: [
                           Container(
@@ -70,16 +134,14 @@ class _RegisterState extends State<Register> {
                                 ],
                               )),
                           Container(
-                              height: MediaQuery.of(context).size.height * 0.63,
+                              height: MediaQuery.of(context).size.height * 0.77,
                               color: ArgonColors.verdeClaro,
                               child: Padding(
-                                padding: const EdgeInsets.all(8.0),
+                                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
                                 child: Center(
                                   child: Column(
                                     crossAxisAlignment:
                                     CrossAxisAlignment.start,
-                                    mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
                                     children: [
                                       Column(
                                         crossAxisAlignment:
@@ -87,36 +149,64 @@ class _RegisterState extends State<Register> {
                                         children: [
                                           Padding(
                                             padding: const EdgeInsets.all(8.0),
-                                            child: Input(
-                                              placeholder: "Nombre",
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                labelText: 'Nombre',
+                                                border: OutlineInputBorder(),
+                                                labelStyle: TextStyle(color: ArgonColors.azul),
+                                                errorText: _errorTexFirstName,
+                                              ),
                                               controller: _firstNameController,
                                             ),
                                           ),
                                           Padding(
                                             padding: const EdgeInsets.all(8.0),
-                                            child: Input(
-                                              placeholder: "Apellido",
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                labelText: 'Apellido',
+                                                border: OutlineInputBorder(),
+                                                labelStyle: TextStyle(color: ArgonColors.azul),
+                                                errorText: _errorTexLastName,
+                                              ),
                                               controller: _lastNameController,
                                             ),
                                           ),
                                           Padding(
                                             padding: const EdgeInsets.all(8.0),
-                                            child: Input(
-                                                placeholder: "Email",
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                labelText: 'Mail',
+                                                  border: OutlineInputBorder(),
+                                                labelStyle: TextStyle(color: ArgonColors.azul),
+                                                errorText: _errorTexEmail,
+                                              ),
                                               controller: _emailController,
+                                              keyboardType: TextInputType.emailAddress,
                                             ),
                                           ),
                                           Padding(
                                             padding: const EdgeInsets.all(8.0),
-                                            child: Input(
-                                                placeholder: "Contraseña",
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                labelText: 'Contraseña',
+                                                border: OutlineInputBorder(),
+                                                labelStyle: TextStyle(color: ArgonColors.azul),
+                                                errorText: _errorTexPassword,
+                                              ),
+                                                obscureText: true,
                                                 controller: _passwordController,
                                             ),
                                           ),
                                           Padding(
                                             padding: const EdgeInsets.all(8.0),
-                                            child: Input(
-                                              placeholder: "Confirmar contraseña",
+                                            child: TextField(
+                                              decoration: InputDecoration(
+                                                labelText: 'Confirmar contraseña',
+                                                border: OutlineInputBorder(),
+                                                labelStyle: TextStyle(color: ArgonColors.azul),
+                                                errorText: _errorTexConfirmPassword,
+                                              ),
+                                              obscureText: true,
                                               controller: _confirmPasswordController,
                                             ),
                                           ),
@@ -127,7 +217,7 @@ class _RegisterState extends State<Register> {
                                         ],
                                       ),
                                       Padding(
-                                        padding: const EdgeInsets.only(top: 16),
+                                        padding: const EdgeInsets.only(top: 10),
                                         child: Center(
                                           child: FlatButton(
                                             textColor: ArgonColors.white,
@@ -142,8 +232,7 @@ class _RegisterState extends State<Register> {
                                               }
 
                                               if (!validEmail(_emailController)) {
-                                                // TODO habria que meter un feedback en rojo algo asi
-                                                return;
+                                                    return;
                                               }
 
                                               if (_passwordController == null || _confirmPasswordController == null) {
@@ -173,8 +262,7 @@ class _RegisterState extends State<Register> {
                                                 padding: EdgeInsets.only(
                                                     left: 16.0,
                                                     right: 16.0,
-                                                    top: 12,
-                                                    bottom: 12),
+                                                    top: 10),
                                                 child: Text("REGISTRARSE",
                                                     style: TextStyle(
                                                         fontWeight:
