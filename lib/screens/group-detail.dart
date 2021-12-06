360ed876bd1638dd010a896b0649c4a73deb200f
Nicole Hinojo <nicole@omniasalud.com>
2021-12-06 05:46:56 -0300

    get info for group detail

--- lib/screens/group-detail.dart
+++ lib/screens/group-detail.dart
@@ -1,95 +1,129 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:greencycle/constants/Theme.dart';
+import 'package:greencycle/model/Group.dart';
+import 'package:greencycle/model/MyUser.dart';
+import 'package:greencycle/services/group_service.dart';
+import 'package:greencycle/services/user_service.dart';
 import 'package:greencycle/widgets/drawer.dart';
 import 'package:greencycle/widgets/navbar.dart';
 
-class GroupDetail extends StatelessWidget {
+final SplayTreeMap<String, String> membersByScore = SplayTreeMap<String, String>();
+int position = 1;
+
+class GroupDetail extends StatefulWidget{
+  @override
+  _CreateGroupDetailState createState() => _CreateGroupDetailState();
+}
+
+class _CreateGroupDetailState extends State<GroupDetail> {
   @override
   Widget build(BuildContext context) {
+    //final groupId = ModalRoute.of(context)!.settings.arguments;
+    // print(groupId);
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: const Text("Detalle grupo"),
         backgroundColor: ArgonColors.verdeOscuro,
       ),
       backgroundColor: ArgonColors.verdeClaro,
-      body: Container(
-          child: SafeArea(
-              child: Column(
-                children: <Widget>[
-                  GroupDetailsTitleWidget(),
-                  Divider(
-                      height: 50,
-                      thickness: 5,
-                      color: ArgonColors.verdeOscuro
-                  ),
-                  ScoreTableWidget(),
-                  Divider(
-                      height: 50,
-                      thickness: 5,
-                      color: ArgonColors.verdeOscuro
-                  ),
-                  ActionsOptionsWidget(),
-                  Divider(
-                      height: 50,
-                      thickness: 5,
-                      color: ArgonColors.verdeOscuro
-                  ),
-                  ParticipantsListWidget(),
-                ],
-              )
-          )
-      ),
-    );
-  }
-  Widget GroupDetailsTitleWidget(){
+      drawer: ArgonDrawer(currentPage: "Detalle grupo"),
+      body: FutureBuilder<Group?>(
+        future: GroupService().getGroupById('pGy9z1cTLqQYTX76SM7D'),
+        builder: (BuildContext context, AsyncSnapshot<Group?> snapshot) {
+          if (snapshot.hasData) {
+            Group? groupData = snapshot.data!;
+            getMembersNames(groupData.members);
+            print(membersByScore);
+            return Container(
+                child: SafeArea(
+                    child: Column(
+                      children: <Widget>[
+                        GroupDetailsTitleWidget(groupData.name),
+                        Divider(
+                            height: 50,
+                            thickness: 5,
+                            color: ArgonColors.verdeOscuro
+                        ),
+                        ScoreTableWidget(),
+                        Divider(
+                            height: 50,
+                            thickness: 5,
+                            color: ArgonColors.verdeOscuro
+                        ),
+                        ParticipantsListWidget(),
+                      ],
+                    )
+                )
+            );
+          } else {
+            return Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(top: 10.0),
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(Colors.white),
+                )
+            );
+          }
+        }));
+    }
+
+  Widget GroupDetailsTitleWidget(String title){
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Padding(padding: const EdgeInsets.only(left: 8.0, top: 32),
-            child: Text("Grupo myGroup", style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 20))
+            child: Text('Grupo $title', style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 20))
         )
       ],
     );
   }
+
   Widget ScoreTableWidget(){
     return Column(
       children: [
-        ScoreTableRowWidget("Azul", "150 Pts", Colors.orange),
-        ScoreTableRowWidget("Felipe", "120 Pts", Colors.blueGrey),
-        ScoreTableRowWidget("Malena", "80 Pts", Colors.brown),
+        for (var score in membersByScore.keys)
+          if(position < 3)
+            ScoreTableRowWidget(membersByScore[score], score.toString(), position++)
       ],
     );
   }
-  Widget ScoreTableRowWidget(String name, String points, Color? color){
+
+  void getMembersNames(Map<String, dynamic> members) async {
+    for (var id in members.keys){
+      var userScoreInGroup = members[id];
+      MyUser? user = await UserService().getUser(id);
+      setState(() {
+        membersByScore[userScoreInGroup] = user!.name;
+      });
+    }
+  }
+  
+  Widget ScoreTableRowWidget(String? name, String score, int position){
+    Color? color = null;
+    switch(position){
+      case 1: color = Colors.orange; break;
+      case 2: color = Colors.blueGrey; break;
+      case 3: color = Colors.brown; break;
+    }
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
         Icon(Icons.stars, color: color, size: 30),
-        Text(name, style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 16)),
-        Text(points, style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 16)),
+        Text(name!, style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 16)),
+        Text(score + ' pts', style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 16)),
       ],
     );
   }
-  Widget ActionsOptionsWidget(){
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Padding(padding: const EdgeInsets.only(left: 9.0),
-            child: Text("Revisar Acciones", style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 18))
-        ),
-      ],
-    );
-  }
+
   Widget ParticipantsListWidget(){
     return Column(
       children: [
-        Text('6 Participantes:', style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 15)),
-        ParticipantsList("Hernán"),
-        ParticipantsList("Malena"),
-        ParticipantsList("Felipe"),
-        ParticipantsList("Florencia"),
-        ParticipantsList("Azul"),
-        ParticipantsList("Nicole"),
+        Text(membersByScore.length.toString() + ' Participantes:', style: TextStyle(fontWeight: FontWeight.bold, color: ArgonColors.azul, fontSize: 15)),
+        for(var memberName in membersByScore.values)
+          ParticipantsList(memberName),
       ],
     );
   }
@@ -102,6 +136,4 @@ class GroupDetail extends StatelessWidget {
       ],
     );
   }
-
-
 }
